581ade8f8f76097b080783ee777895e7d6d8b261
pradeek <pradeek.mohandas@com>
2020-08-02 00:34:52 +0400

    error handling

--- src/main/java/com/pradeek/demo/kafka/Consumer.java
+++ src/main/java/com/pradeek/demo/kafka/Consumer.java
@@ -1,15 +1,14 @@
 package com.pradeek.demo.kafka;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.ConsumerAwareListenerErrorHandler;
-import org.springframework.kafka.listener.ListenerExecutionFailedException;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import org.springframework.kafka.support.KafkaHeaders;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pradeek.demo.model.Student;
 import lombok.extern.log4j.Log4j2;
 
@@ -21,10 +20,16 @@ public class Consumer {
 
 
 	@KafkaHandler
-	 public void consume(Student student,@Header(KafkaHeaders.OFFSET) String offset){
+	 public void consume(@Payload String student,@Header(KafkaHeaders.OFFSET) String offset){
 	    log.info(" offset: " + offset + "Incoming info: "+student);
+	      try {
+	            ObjectMapper mapper = new ObjectMapper();
+	            Student json = mapper.readValue(student, Student.class);
+	            log.info("Json  :"+json);
+	        } catch (JsonProcessingException e) {
+	            e.printStackTrace();
+	            log.error(e.getMessage());
+	        }
+	    
  	}
-	
-
-
 }
